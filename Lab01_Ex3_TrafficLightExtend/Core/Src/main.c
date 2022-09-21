f85f0195ed3ff7ef515a6e7883ae378b9a1b6c72
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-09-21 14:05:51 +0700

    Build project for proteus

--- Lab01_Ex3_TrafficLightExtend/Core/Src/main.c
+++ Lab01_Ex3_TrafficLightExtend/Core/Src/main.c
@@ -88,6 +88,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   int led_stat_1 = 0, led_stat_2 = 2; //0: turn GREEN on, 1: turn YELLOW on, 2: turn RED on
   int counter_1 = 0, counter_2 = 0; //(3: GREEN stays, 2: YELLOW stays, 5: RED stays) * 1s
+  const int counter_lst[3] = {3, 2, 5}; //(5: GREEN stays, 2: YELLOW stays, 3: RED stays) * 1000ms
   /* USER CODE END 2 */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -96,23 +97,21 @@ int main(void)
 	  /* USER CODE END WHILE */
 	  /* USER CODE BEGIN 3 */
 	  if (counter_1==0){ // Check first traffic light
+	  	  counter_1 = counter_lst[led_stat_1];
 		  switch(led_stat_1){
 		  	  case 0:{
-		  		  counter_1 = 3;
 		  		  led_stat_1 = 1;
 		  		  HAL_GPIO_WritePin(GPIOA, LED_GREEN_1_Pin, GPIO_PIN_RESET);
 		  		  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_1_Pin | LED_RED_1_Pin, GPIO_PIN_SET);
 		  		  break;
 		  	  }
 		  	  case 1:{
-		  		  counter_1 = 2;
 		  		  led_stat_1 = 2;
 		  		  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_1_Pin, GPIO_PIN_RESET);
 		  		  HAL_GPIO_WritePin(GPIOA, LED_RED_1_Pin | LED_GREEN_1_Pin, GPIO_PIN_SET);
 		  		  break;
 		  	  }
 		  	  case 2:{
-		  		  counter_1 = 5;
 		  		  led_stat_1 = 0;
 		  		  HAL_GPIO_WritePin(GPIOA, LED_RED_1_Pin, GPIO_PIN_RESET);
 		  		  HAL_GPIO_WritePin(GPIOA, LED_GREEN_1_Pin | LED_YELLOW_1_Pin, GPIO_PIN_SET);
@@ -121,23 +120,21 @@ int main(void)
 		  }
 	  }
 	  if (counter_2==0){ // Check second traffic light
+		  counter_2 = counter_lst[led_stat_2];
 		  switch(led_stat_2){
 			  case 0:{
-				  counter_2 = 3;
 				  led_stat_2 = 1;
 				  HAL_GPIO_WritePin(GPIOA, LED_GREEN_2_Pin, GPIO_PIN_RESET);
 				  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_2_Pin | LED_RED_2_Pin, GPIO_PIN_SET);
 				  break;
 			  }
 			  case 1:{
-				  counter_2 = 2;
 				  led_stat_2 = 2;
 				  HAL_GPIO_WritePin(GPIOA, LED_YELLOW_2_Pin, GPIO_PIN_RESET);
 				  HAL_GPIO_WritePin(GPIOA, LED_RED_2_Pin | LED_GREEN_2_Pin, GPIO_PIN_SET);
 				  break;
 			  }
 			  case 2:{
-				  counter_2 = 5;
 				  led_stat_2 = 0;
 				  HAL_GPIO_WritePin(GPIOA, LED_RED_2_Pin, GPIO_PIN_RESET);
 				  HAL_GPIO_WritePin(GPIOA, LED_GREEN_2_Pin | LED_YELLOW_2_Pin, GPIO_PIN_SET);
